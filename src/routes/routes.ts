295f4af31c0fba2c1df853793431668fee3b4e53
Abdallah Aboudeif <71098171+aboudeif@users.noreply.github.com>
2022-07-22 17:13:19 +0200

    fix formatting

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -23,7 +23,11 @@ routes.get(
     const info = getThumbInfo(req)
     if (!fs.existsSync(info[0])) {
       const response = await resizeImage(info)
-      response ? res.status(200).sendFile(info[0]) : res.status(500).send(errormsg('image resize faild', 'could not resize image '+info[1], 'try to resize it again'))
+      response
+        ? res.status(200).sendFile(info[0])
+        : res
+            .status(500)
+            .send(errormsg('image resize faild', 'could not resize image ' + info[1], 'try to resize it again'))
     } else {
       res.status(200).sendFile(info[0])
     }
@@ -39,18 +43,29 @@ routes.get('/api/images', (req: express.Request, res: express.Response): void =>
 // return the selected image to home.html page
 routes.get('/api/show', (req: express.Request, res: express.Response): void => {
   const image = path.join(__dirname, '../../storage/images', `${req.query.img}`)
-  image ? res.status(200).sendFile(image) : res.status(500).send(errormsg('image not found', 'there is no image with name: ' + req.query.img, 'try to upload it first'))
+  image
+    ? res.status(200).sendFile(image)
+    : res
+        .status(500)
+        .send(errormsg('image not found', 'there is no image with name: ' + req.query.img, 'try to upload it first'))
 })
 
 // upload image to storage/images folder
-routes.post('/upload', upload.single('uploaded_file'), async function (req: express.Request, res: express.Response): Promise<void> {
+routes.post('/upload', upload.single('uploaded_file'), async function (
+  req: express.Request,
+  res: express.Response
+): Promise<void> {
   const response = uploadImage(req)
-  response ? res.redirect('/') : res.status(500).send(errormsg('image upload faild', 'could not upload image', 'try to upload it again'))
-} )
+  response
+    ? res.redirect('/')
+    : res.status(500).send(errormsg('image upload faild', 'could not upload image', 'try to upload it again'))
+})
 
 // not found page
 routes.get('*', (req: express.Request, res: express.Response): void => {
-  res.status(404).send(errormsg('page not found', 'there is no path lead to here', `try to back to <a href='/'>home</a> page` ))
+  res
+    .status(404)
+    .send(errormsg('page not found', 'there is no path lead to here', `try to back to <a href='/'>home</a> page`))
 })
 
 export default routes
